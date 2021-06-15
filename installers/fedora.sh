aeb81bec3671061a2861e5a48b20d3fc0d81eaa1
Hendrik Roth <hi@hendrikroth.com>
2021-06-15 14:40:48 +0200

    colors

--- installers/fedora.sh
+++ installers/fedora.sh
@@ -1,47 +1,60 @@
 #!/bin/sh
 
 sudo dnf -y install \
-  xrandr \
-	git \
 	@development-tools \
-	bluez \
-	p7zip \
-	unzip \
-	curl \
-	wget \
-	xz \
-	vim \
-	emacs \
-	sxhkd \
-	bspwm \
-	dmenu \
-	xorg-x11-server-Xorg \
-	xorg-x11-font-utils \
-	xorg-x11-xinit \
-	xorg-x11-drv-* \
-	mesa-dri-drivers \
 	alacritty \
+	bat \
+	bluez \
+	bspwm \
 	chromium \
+	curl \
+	dmenu \
+	dnf-plugins-core \
+	emacs \
+	exa \
+	fira-code-fonts \
 	firefox \
 	fontconfig \
-	maildir-utils \
 	fzf \
-	zsh \
-	texlive-scheme-full \
-	fira-code-fonts \
-	inkscape \
 	gimp \
+	git \
+	inkscape \
+  isync \
+	maildir-utils \
+	mesa-dri-drivers \
+	neovim \
+	nnn \
+	p7zip \
 	playerctl \
-	dnf-plugins-core
+	ripgrep \
+	sxhkd \
+	texlive-scheme-medium \
+	unzip \
+	wget \
+	xorg-x11-drv-* \
+	xorg-x11-font-utils \
+	xorg-x11-server-Xorg \
+	xorg-x11-xinit \
+	xrandr \
+	xz \
+	zsh \
+  direnv \
+  dunst \
+  google-noto-sans-fonts \
+  google-roboto-fonts \
+  pass \
+  polybar \
+  scrot \
+  udiskie
 
 # Docker
 sudo dnf config-manager \
 	--add-repo https://download.docker.com/linux/fedora/docker-ce.repo
 
 sudo dnf -y install \
+	containerd.io \
 	docker-ce \
-	docker-ce-cli \
-	containerd.io
+	docker-ce-cli
 
 # Guix
 sudo dnf config-manager \
@@ -50,23 +63,27 @@ sudo dnf config-manager \
 sudo dnf -y install guix
 
 # Services
+sudo systemctl enable bluetooth
 sudo systemctl enable dbus-broker
 sudo systemctl enable docker
-sudo systemctl enable bluetooth
 sudo systemctl enable guix-daemon
 
+sudo systemctl start bluetooth
 sudo systemctl start dbus-broker
 sudo systemctl start docker
-sudo systemctl start bluetooth
 sudo systemctl start guix-daemon
 
 # Groups
 sudo gpasswd -a `whoami` guixbuild
+
+# Default zsh shell
+sudo usermod -s /bin/zsh `whoami`
 
 # Fonts
 git clone https://github.com/edwardtufte/et-book tmp
 mkdir -p ~/.fonts
 cp -r tmp/et-book-ligatures-enabled/**/*.{otf,woff,woff2} ~/.fonts
 rm -rf tmp
+fc-cache -f
 
-pip install pywal
+sudo pip install pywal
